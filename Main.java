e13bb1cbdb99bb31cf07e23634b82bf25c57444d
Mohsine Zireg <mohsine.zireg@student-cs.fr>
2023-09-26 19:14:21 +0200

    fix: test

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, this is bogboii!!");
+        System.out.println("Hello, this is bogboi testing git push master!!");
     }
 }
 
